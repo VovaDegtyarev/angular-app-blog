25070abf80e01a5fb89d1b13a9f943e6fc52326a
Vladimir Degtyarev <vladimir1860921@gmail.com>
2020-04-24 17:12:45 +0300

    added work with token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCIwNQBggL0X_RpkRO91G5GY3LUeHpGVg4'
 };
